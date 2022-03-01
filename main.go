fa4ad95b43898224866814190d870eca708a6923
Matic Drobez <matic.drobez@gmail.com>
2022-03-01 14:03:05 +0100

    Remove else statment.

--- main.go
+++ main.go
@@ -20,17 +20,17 @@ var errReadingInput = errors.New("Error reading input")
 func main() {
 	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
-	} else {
-		originUnit = strings.ToUpper(os.Args[1])
-		for {
-			fmt.Print("What is the current temperature in " + originUnit + " ? ")
+	}
 
-			fmt.Print("Would you like to convert another temperature ? (y/n) ")
+	originUnit = strings.ToUpper(os.Args[1])
+	for {
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
-			if shouldConvertAgain != "Y" {
-				fmt.Println("Good bye!")
-				break
-			}
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		if shouldConvertAgain != "Y" {
+			fmt.Println("Good bye!")
+			break
 		}
 	}
 }
